fb60315982ff0817076f02dd88ec84130e5778a3
easonyzw <101691537+easonyzw@users.noreply.github.com>
2026-05-07 18:11:23 +0800

    Update main.c

--- User/main.c
+++ User/main.c
@@ -1,28 +1,16 @@
-#include "stm32f10x.h"                  // Device header
-#include "Delay.h"
+#include "stm32f10x.h"
 #include "OLED.h"
-#include "Timer.h"
-
-uint16_t Num;
+#include "gps.h"
 
 int main(void)
 {
 	OLED_Init();
-	Timer_Init();
-	
-	OLED_ShowString(1, 1, "Num:");
-	
+	GPS_Init(9600);
+
+	OLED_ShowString(1, 1, "GPS Init...");
+
 	while (1)
 	{
-		OLED_ShowNum(1, 5, Num, 5);
-	}
-}
-
-void TIM2_IRQHandler(void)
-{
-	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
-	{
-		Num ++;
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+		GPS_ShowOnOLED();
 	}
 }
